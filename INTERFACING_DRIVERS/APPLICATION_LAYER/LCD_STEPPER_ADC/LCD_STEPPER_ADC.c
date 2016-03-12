bcf1ca6414888fe9106b90dfcd5045592fbca29f
Ahmed <Ahmed@Ahmed-PC>
2016-03-12 06:38:53 +0200

    sync

---
+++ INTERFACING_DRIVERS/APPLICATION_LAYER/LCD_STEPPER_ADC/LCD_STEPPER_ADC.c
@@ -0,0 +1,38 @@
+#include"../types.h"
+#include"../../MCAL_LAYER/DIO_DRIVER/DIO_interface.h"
+#include"../../MCAL_LAYER/ADC_DRIVER/ADC_interface.h"
+#include"../../HAL_LAYER/CHARACTER_LCD/CHARACTER_LCD_interface.h"
+
+ int main()
+     {
+
+     DIO_voidInit();
+
+     CHARACTER_LCD_voidInit();
+
+     //goto cgram
+     CHARACTER_LCD_voidWriteCommand(0b01000000);
+
+     CHARACTER_LCD_voidWriteData(0xff);
+     CHARACTER_LCD_voidWriteData(0x00);
+     CHARACTER_LCD_voidWriteData(0xff);
+     CHARACTER_LCD_voidWriteData(0x00);
+
+     CHARACTER_LCD_voidWriteData(0xff);
+     CHARACTER_LCD_voidWriteData(0x00);
+     CHARACTER_LCD_voidWriteData(0xff);
+     CHARACTER_LCD_voidWriteData(0x00);
+//goto ddram
+     //CHARACTER_LCD_voidWriteCommand(0x80);
+//     CHARACTER_LCD_voidWriteCommand(0b11000000);  start of line 2
+
+     CHARACTER_LCD_voidWriteCommand(0b11000000);
+
+
+     CHARACTER_LCD_voidWriteData(0x00);
+
+
+     return 0;
+
+     }
+
