c1691c913973e18b84ef6c0c085ac6bf6b29299c
Ahmed <Ahmed@Ahmed-PC>
2016-03-14 10:00:12 +0200

    hh

--- INTERFACING_DRIVERS/APPLICATION_LAYER/LCD_STEPPER_ADC/LCD_STEPPER_ADC.c
+++ INTERFACING_DRIVERS/APPLICATION_LAYER/LCD_STEPPER_ADC/LCD_STEPPER_ADC.c
@@ -3,11 +3,13 @@
 #include"../../MCAL_LAYER/ADC_DRIVER/ADC_interface.h"
 #include"../../HAL_LAYER/CHARACTER_LCD/CHARACTER_LCD_interface.h"
 #include "../../HAL_LAYER/SREPPER_MOTOR/STEPPER_MOTOR_interface.h"
-
+#include "../../HAL_LAYER/TACTILE_SWITCH/TACTILE_SWITCH_interface.h"
+//#define speed_limit 4
 int main()
     {
-
+    u8 switch_result;
     u16 temp;
+    u8 counter;
 
     DIO_voidInit();
 
@@ -15,9 +17,11 @@ int main()
     STEPPER_MOTOR_voidInit();
     CHARACTER_LCD_voidInit();
 
+    TACTILE_SWITCH_voidInit();
+
     ADC_voidEnable();
     //goto cgram
-  //  CHARACTER_LCD_voidWriteCommand(0b01000000);
+    //  CHARACTER_LCD_voidWriteCommand(0b01000000);
 //
 //    CHARACTER_LCD_voidWriteData(0xff);
 //    CHARACTER_LCD_voidWriteData(0x00);
@@ -33,23 +37,177 @@ int main()
 //
 //    CHARACTER_LCD_voidWriteData(0x00);
 
+    DIO_u8WritePortDir(DIO_u8PORT2, 0xff);
+
     while (1)
 	{
 
-	ADC_u8Read_Channel_One_Shot(ADC_u8Channel0, &temp);
+	TACTILE_SWITCH_u8Read(TACTILE_SWITCH_u8SWITCH1ID,&switch_result);
 
+	counter++;
+	ADC_u8Read_Channel_One_Shot(ADC_u8Channel0, &temp);
 
 	if (temp < 128)
 	    {
-	    STEPPER_MOTOR_RotateLeft();
-	   // CHARACTER_LCD_voidWrite("LEFT ",CHARACTER_LCD_u8LINE_1,0);
+	    if (switch_result == TACTILE_SWITCH_u8PRESSED)
+		{
+		CHARACTER_LCD_voidWrite("hamo ", CHARACTER_LCD_u8LINE_1, 0);
+		}
+	    else
+		{
+		CHARACTER_LCD_voidWrite("LEFT ", CHARACTER_LCD_u8LINE_1, 0);
+		}
+
+	    switch ((u8) temp / (u8) 26)
+		{
+
+	    case 0:
+
+		CHARACTER_LCD_voidWrite("case 0 ", CHARACTER_LCD_u8LINE_2, 0);
+
+		if (counter == 1)
+		    {
+		    STEPPER_MOTOR_RotateLeft();
+		    counter = 0;
+		    }
+		else
+		    {
+
+		    }
+		break;
+	    case 1:
+		CHARACTER_LCD_voidWrite("case 1 ", CHARACTER_LCD_u8LINE_2, 0);
+
+		if (counter == 2)
+		    {
+		    STEPPER_MOTOR_RotateLeft();
+		    counter = 0;
+		    }
+		else
+		    {
+
+		    }
+		break;
+	    case 2:
+		CHARACTER_LCD_voidWrite("case 2 ", CHARACTER_LCD_u8LINE_2, 0);
+
+		if (counter == 3)
+		    {
+		    STEPPER_MOTOR_RotateLeft();
+		    counter = 0;
+		    }
+		else
+		    {
+
+		    }
+		break;
+	    case 3:
+		CHARACTER_LCD_voidWrite("case 3 ", CHARACTER_LCD_u8LINE_2, 0);
+
+		if (counter == 4)
+		    {
+		    STEPPER_MOTOR_RotateLeft();
+		    counter = 0;
+		    }
+		else
+		    {
+
+		    }
+		break;
+	    case 4:
+		CHARACTER_LCD_voidWrite("case 4 ", CHARACTER_LCD_u8LINE_2, 0);
+
+		break;
+	    default:
+		CHARACTER_LCD_voidWrite("case D ", CHARACTER_LCD_u8LINE_2, 0);
+
+		break;
+
+		}
 	    }
 	else
 	    {
-	    STEPPER_MOTOR_RotateRight();
-	 //   CHARACTER_LCD_voidWrite("RIGHT",CHARACTER_LCD_u8LINE_1,6);
-	   // CHARACTER_LCD_voidClear(CHARACTER_LCD_u8LINE_1);
+
+	    if (switch_result == TACTILE_SWITCH_u8PRESSED)
+		{
+		CHARACTER_LCD_voidWrite("hamo ", CHARACTER_LCD_u8LINE_1, 0);
+		}
+	    else
+		{
+		CHARACTER_LCD_voidWrite("RIGHT", CHARACTER_LCD_u8LINE_1, 0);
+		}
+	    temp = temp - 127;
+
+	    switch ((u8)4-((u8) temp / (u8) 26))
+		{
+
+	    case 0:
+		CHARACTER_LCD_voidWrite("case 0 ", CHARACTER_LCD_u8LINE_2, 0);
+
+		if (counter == 1)
+		    {
+		    STEPPER_MOTOR_RotateRight();
+		    counter = 0;
+		    }
+		else
+		    {
+
+		    }
+		break;
+	    case 1:
+
+		CHARACTER_LCD_voidWrite("case 1 ", CHARACTER_LCD_u8LINE_2, 0);
+
+		if (counter == 2)
+		    {
+		    STEPPER_MOTOR_RotateRight();
+		    counter = 0;
+		    }
+		else
+		    {
+
+		    }
+		break;
+	    case 2:
+		CHARACTER_LCD_voidWrite("case 2 ", CHARACTER_LCD_u8LINE_2, 0);
+
+		if (counter == 3)
+		    {
+		    STEPPER_MOTOR_RotateRight();
+		    counter = 0;
+		    }
+		else
+		    {
+
+		    }
+		break;
+	    case 3:
+		CHARACTER_LCD_voidWrite("case 3 ", CHARACTER_LCD_u8LINE_2, 0);
+
+		if (counter == 4)
+		    {
+		    STEPPER_MOTOR_RotateRight();
+		    counter = 0;
+		    }
+		else
+		    {
+
+		    }
+		break;
+	    case 4:
+		CHARACTER_LCD_voidWrite("case 4 ", CHARACTER_LCD_u8LINE_2, 0);
+
+		break;
+	    default:
+		CHARACTER_LCD_voidWrite("case D ", CHARACTER_LCD_u8LINE_2, 0);
+
+		break;
+
+		}
+
 	    }
+	DIO_u8WritePortVal(DIO_u8PORT2, (u8) temp / (u8) 26);
+
 	}
 
     return 0;
