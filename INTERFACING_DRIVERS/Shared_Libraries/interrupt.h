b28d3518d7cafed7aa339deaaf9c0e46a4c90ec6
Ahmed <Ahmed@Ahmed-PC>
2016-04-14 18:28:59 +0200

    Adding Enable and Disable External interrupt features

--- INTERFACING_DRIVERS/Shared_Libraries/interrupt.h
+++ INTERFACING_DRIVERS/Shared_Libraries/interrupt.h
@@ -12,5 +12,8 @@
 		void vector(void) __attribute__ ((signal,used,externally_visible)) __VA_ARGS__;\
 		void vector(void)
 
+#define Enable_Global_INT()       __asm("SEI")
+
+#define Disable_Global_INT()      __asm("CLI")
 
 #endif /* SHARED_LIBRARIES_INTERRUPT_H_ */
