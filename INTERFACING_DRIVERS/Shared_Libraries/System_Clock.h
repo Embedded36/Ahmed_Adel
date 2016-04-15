740d191753db7690db6a8b64db70f22d8a367376
Ahmed <Ahmed@Ahmed-PC>
2016-04-15 09:34:38 +0200

    Create Sperate file  for system clock to be shared between all modules

---
+++ INTERFACING_DRIVERS/Shared_Libraries/System_Clock.h
@@ -0,0 +1,14 @@
+/*
+ * System_Clock.h
+ *
+ *  Created on: Apr 14, 2016
+ *      Author: Ahmed
+ */
+
+#ifndef SHARED_LIBRARIES_SYSTEM_CLOCK_H_
+#define SHARED_LIBRARIES_SYSTEM_CLOCK_H_
+
+/*Comment!: System clock */
+#define F_CPU                4000000UL
+
+#endif /* SHARED_LIBRARIES_SYSTEM_CLOCK_H_ */
